c0f8a0b6745a3b15dd22735339fa178acbfa11df
siddhi <siddhideshmukh06@gmail.com>
2025-07-05 21:41:44 +0530

    Update queue.c

--- queue.c
+++ queue.c
@@ -16,7 +16,7 @@ int rq_init(rq_t *q) {
 int rq_enqueue(rq_t *q, int value) {
     int next_tail = (q->tail + 1) % MAX_QUEUE_LEN;
     if (next_tail == q->head) {
-        return -1; 
+        return -1; // Queue full
     }
     q->value[q->tail] = value;
     q->tail = next_tail;
@@ -25,7 +25,7 @@ int rq_enqueue(rq_t *q, int value) {
 
 int rq_dequeue(rq_t *q, int *value) {
     if (q->head == q->tail) {
-        return -1; 
+        return -1; // Queue empty
     }
     *value = q->value[q->head];
     q->head = (q->head + 1) % MAX_QUEUE_LEN;
@@ -35,23 +35,41 @@ int rq_dequeue(rq_t *q, int *value) {
 int main() {
     rq_t q;
     rq_init(&q);
-    int n, val;
-    printf("Enter number of elements to enqueue: ");
-    scanf("%d", &n);
+    int choice, val;
 
-    printf("Enter %d elements:\n", n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &val);
-        if (rq_enqueue(&q, val) == -1) {
-            printf("Queue full, can't add more.\n");
-            break;
+    while (1) {
+        printf("\n---- Queue Menu ----\n");
+        printf("1. Enqueue\n");
+        printf("2. Dequeue\n");
+        printf("3. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter value to enqueue: ");
+                scanf("%d", &val);
+                if (rq_enqueue(&q, val) == -1) {
+                    printf("Queue is full,Cannot enqueue.\n");
+                } else {
+                    printf("Enqueued %d successfully.\n", val);
+                }
+                break;
+
+            case 2:
+                if (rq_dequeue(&q, &val) == -1) {
+                    printf("Queue is empty, Cannot dequeue.\n");
+                } else {
+                    printf("Dequeued value: %d\n", val);
+                }
+                break;
+
+            case 3:
+                printf("Exiting program.\n");
+                return 0;
+
+            default:
+                printf("Invalid choice\n");
         }
     }
-
-    printf("Dequeued elements:\n");
-    while (rq_dequeue(&q, &val) == 0) {
-        printf("%d\n", val);
-    }
-
-    return 0;
 }
