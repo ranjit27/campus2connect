197b5456f1b17e880a892436e96cc7662a5de919
AnishSapre <anish.sapre654@gmail.com>
2025-07-06 23:21:29 +0530

    file operations test

---
+++ Buffer Problem/Practice Codes/file_ops.c
@@ -0,0 +1,34 @@
+//
+// Created by Anish Sapre on 06/07/25.
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    // File pointer
+    FILE* fptr;
+
+    // Get the data to be written in file
+    char data[50] = "ANISHANISHANISHANISHANISHANISH";
+
+    // Creating file using fopen()
+    // with access mode "w"
+    fptr = fopen("Buffer Problem/Practice Codes/file.txt", "w");
+
+    // Checking if the file is created
+    if (fptr == NULL)
+        printf("The file is not opened.");
+    else{
+        printf("The file is now opened.\n");
+        fputs(data, fptr);
+        fputs("\n", fptr);
+
+        // Closing the file using fclose()
+        fclose(fptr);
+        printf("Data successfully written in file "
+               "file.txt\n");
+        printf("The file is now closed.");
+    }
+    return 0;
+}
