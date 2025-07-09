1e9e2a1d038c6d8580e981a2646e6ede2f72d0af
SonaliRShende <165500161+SonaliRShende@users.noreply.github.com>
2025-07-09 23:44:52 +0530

    Made changes to buffer list code

--- SonaliShende/Linked_list/test.c
+++ SonaliShende/Linked_list/test.c
@@ -39,41 +39,48 @@ struct fixed_buff* fixed_buff_alloc(void)
 }
 
 struct fixed_buff* build_fixed_buff_list(struct stream *s);
-
-
-int main() {
+int main(int argc, char *argv[]) {
+    FILE *input = stdin;
     FILE *log = fopen("output.log", "w");
     if (!log) {
-        printf("Could not open log file\n");
+        perror("Log file error");
         return 1;
     }
 
-    printf("Enter input stream (Ctrl+D to end input):\n");
-
-    int ch;
-    while ((ch = getchar()) != EOF && total_len < sizeof(user_data)) {
-        user_data[total_len++] = (uint8_t)ch;
+    if (argc == 2) {
+        input = fopen(argv[1], "rb");
+        if (!input) {
+            fprintf(stderr, "Could not open file: %s\n", argv[1]);
+            fclose(log);
+            return 1;
+        }
+    } else {
+        printf("Enter input stream (Ctrl+D to end input):\n");
     }
 
-    struct stream s;
-    struct fixed_buff *head = build_fixed_buff_list(&s);
+    int ch;
+    while ((ch = fgetc(input)) != EOF && total_len < sizeof(user_data)) {
+        user_data[total_len++] = (uint8_t)ch;
+    }
+    if (input != stdin) fclose(input);
 
-    int count = 1;
-    while (head) {
-        fprintf(log, "Buffer %d - data_len: %u\n", count++, head->data_len);
-        for (unsigned int i = 0; i < head->data_len; i++) {
-            fprintf(log, "%u ", head->data[i]);
+    struct stream s;
+    struct fixed_buff *node = build_fixed_buff_list(&s);
+
+    for (int i = 1; node; i++) {
+        fprintf(log, "Buffer %d - data_len: %u\n", i, node->data_len);
+        for (unsigned int j = 0; j < node->data_len; j++) {
+            fprintf(log, "%u ", node->data[j]);
         }
         fprintf(log, "\n");
 
-        struct fixed_buff *tmp = head;
-        head = head->next;
-        free(tmp);
+        struct fixed_buff *temp = node;
+        node = node->next;
+        free(temp);
     }
 
     fprintf(log, "Finished reading stream.\n");
     fclose(log);
     printf("Check output.log for result.\n");
-
     return 0;
 }
